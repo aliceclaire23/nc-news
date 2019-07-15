16a82d725676f8bbdc94e5ea555fd1efce922c91
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-15 16:52:36 +0100

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    topics: [],
+    selectedTopic: ''
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Nav />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
