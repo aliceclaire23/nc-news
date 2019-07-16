245e44d8d5f9a1a653836360ac92aa95cace0b4d
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-16 11:14:56 +0100

    amend articles component to a class

---
+++ src/components/Articles.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Articles extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h2>Article(s) go here...</h2>
+      </div>
+    );
+  }
+}
+
+export default Articles;
