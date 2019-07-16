daaa8dac9afa9ade51fc9b597dca95f7c04fe1a2
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-16 11:56:40 +0100

    add selectedTopic  to Articles header

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,9 +4,11 @@ class Articles extends Component {
   state = {};
 
   render() {
+    const { topic } = this.props;
+    console.log(topic);
     return (
       <div>
-        <h2>Article(s) go here...</h2>
+        <h2>{topic ? `Articles about ${topic}` : 'All Articles'}</h2>
       </div>
     );
   }
