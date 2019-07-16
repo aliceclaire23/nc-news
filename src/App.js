4c1b366e0d3bad994da4204735010d492c8c3233
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-16 11:14:36 +0100

    add router and fetch topics top app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Router } from '@reach/router';
+
 import './App.css';
+
 import Header from './components/Header';
 import Nav from './components/Nav';
-import Main from './components/Main';
+import Articles from './components/Articles';
 import Footer from './components/Footer';
 
 class App extends Component {
@@ -12,15 +16,33 @@ class App extends Component {
   };
 
   render() {
+    const { topics } = this.state;
     return (
       <div className='App'>
         <Header />
-        <Nav />
-        <Main />
+        <Nav topics={topics} />
+        <Router className='main'>
+          <Articles path='/' />
+        </Router>
         <Footer />
       </div>
     );
   }
+
+  componentDidMount = () => {
+    this.fetchTopics().then(topics => {
+      this.setState({ topics });
+    });
+  };
+
+  fetchTopics = async () => {
+    const { data } = await axios.get(
+      `https://ecila-nc-news-api.herokuapp.com/api/topics/`
+    );
+    return data.topics;
+  };
+
+  updateSelectedTopic = topic => {};
 }
 
 export default App;
