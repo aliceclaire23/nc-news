a348a362a0dcbe188d490fc03f4761f056b77085
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-19 11:31:29 +0100

    add links to footer and foot a tage styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,17 @@ function Footer(props) {
   return (
     <div className='footer'>
       <ul>
-        <li className='footer-item'>Github</li>
-        <li className='footer-item'>CodePen</li>
-        <li className='footer-item'>Linked In</li>
+        <li className='footer-item'>
+          <a href='https://github.com/Ecila23'>Github</a>
+        </li>
+        <li className='footer-item'>
+          <a href='https://codepen.io/ecila23/#'>CodePen</a>
+        </li>
+        <li className='footer-item'>
+          <a href='https://www.linkedin.com/in/alice-claire-earle-4163a3171'>
+            Linked In
+          </a>
+        </li>
       </ul>
     </div>
   );
